f52a88f29f10204ab70697f7b695b8149e48bf9b
s24raksagne <rakstinaagneta@gmail.com>
2026-04-30 11:50:09 +0300

    Started making the add function

--- src/datastr/MyBST.java
+++ src/datastr/MyBST.java
@@ -25,7 +25,25 @@ public class MyBST<Ttype> { //my binary search tree
 		}
 	}
 	
+	public void add(Ttype element) throws Exception {
+		if (isFull()) {
+			throw new Exception ("Koks ir pilens un nevar vairs pievienot jauus elementus");
+		}
+		
+		addHelper(rootNode, element);
+		howManyElements--;
+	}
 	
-	
+	private void addHelper(MyNode<Ttype> nodeTemp, Ttype element) {
+		if (nodeTemp != null) {
+			
+			if (((Comparable)element).compareTo(nodeTemp.getElement()) >0) {
+				addHelper(nodeTemp.getRightChNode(), element);
+			} else {
+				addHelper(nodeTemp.getLeftChNode(), element);
+			}
+			//TODO pabeigt funkciju
+		}
+	}
 	
 }
